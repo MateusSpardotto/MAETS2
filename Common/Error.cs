19b57f731da2c744fd923dd9ebf69f2d800b1b00
Mateus Spardotto <mateus.spardotto@gmail.com>
2020-03-06 15:08:35 -0300

    Commom criado. Validações de CPF e Email.

---
+++ Common/Error.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common
+{
+    public class Error
+    {
+        public string Message { get; set; }
+        public string FieldName { get; set; }
+    }
+}
---
+++ Common/MSException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Common
+{
+    [Serializable]
+    public class MSException : Exception
+    {
+        public List<Error> Errors { get; private set; }
+
+        public MSException(List<Error> errors) 
+        {
+            this.Errors = errors;
+        }
+
+        public MSException(string message) : base(message) { }
+        public MSException(string message, Exception inner) : base(message, inner) { }
+        protected MSException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
