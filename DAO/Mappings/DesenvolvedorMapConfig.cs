a9e01782806b08469f0c9719005be98b56e03469
Nataan' ॐ <natanluis2011@hotmail.com>
2020-03-09 17:04:04 -0300

    MapConfigs criadas

---
+++ DAO/Mappings/DesenvolvedorMapConfig.cs
@@ -0,0 +1,26 @@
+﻿using DTO;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Text;
+
+namespace DAO.Mappings
+{
+    public class DesenvolvedorMapConfig : EntityTypeConfiguration<DesenvolvedorDTO>
+    {
+        public DesenvolvedorMapConfig()
+        {
+            this.ToTable("DESENVOLVEDORES");
+
+            this.Property(d => d.Nome)
+                .HasMaxLength(150)
+                .IsRequired()
+                .IsUnicode();
+
+            this.Property(d => d.PaisOrigem)
+                .HasMaxLength(80)
+                .IsRequired()
+                .IsUnicode();
+        }
+    }
+}
---
+++ DAO/Mappings/UsuarioMapConfig.cs
@@ -0,0 +1,52 @@
+﻿using DTO;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Text;
+
+namespace DAO.Mappings
+{
+    public class UsuarioMapConfig : EntityTypeConfiguration<UsuarioDTO>
+    {
+        public UsuarioMapConfig()
+        {
+            this.ToTable("USUARIOS");
+
+            this.Property(u => u.Nome)
+                .HasMaxLength(60)
+                .IsRequired()
+                .IsUnicode();
+
+            this.Property(u => u.Email)
+                .HasMaxLength(60)
+                .IsRequired()
+                .IsUnicode();
+            this.HasIndex(u => u.Email)
+                .IsUnique();
+
+            this.Property(u => u.Senha)
+                .HasMaxLength(16)
+                .IsRequired()
+                .IsUnicode();
+            this.HasIndex(u => u.Senha)
+                .GetHashCode();
+
+            this.Property(u => u.CPF)
+                .IsFixedLength()
+                .HasMaxLength(14)
+                .IsRequired()
+                .IsUnicode();
+            this.HasIndex(u => u.CPF)
+                .IsUnique();
+
+            this.Property(u => u.DataNascimento)
+                .IsRequired()
+                .HasColumnType("date");
+
+            this.Property(u => u.Pais)
+                .HasMaxLength(80)
+                .IsRequired()
+                .IsUnicode();
+        }
+    }
+}
