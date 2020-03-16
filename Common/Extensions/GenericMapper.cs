e984b7fc6c89a0b433ac0939c66d50601d68d0c3
Nataan' ॐ <natanluis2011@hotmail.com>
2020-03-16 14:11:43 -0300

    blabla só commitando pra dar pull nos outros commits

---
+++ Common/Extensions/GenericMapper.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Common.Extensions
+{
+    public static class GenericMapper
+    {
+        public static List<Target> ToViewModel<Source,Target>(this IEnumerable<Source> data)
+        {
+            List<Target> targetData = new List<Target>();
+
+            var configuration = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<Source, Target>();
+            });
+            IMapper mapper = configuration.CreateMapper();
+
+            foreach (Source item in data)
+            {
+                targetData.Add(mapper.Map<Source, Target>(item));
+            }
+            return targetData;
+        }
+    }
+}
