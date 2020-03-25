8c1041b56269a26c35d3f54fedc73d68ff546b51
Nataan' ॐ <luisnataan556@gmail.com>
2020-03-25 14:15:23 -0300

    Carteira.cshtml CarteiraQueryViewModel

---
+++ MAETS/Models/Query/CarteiraQueryViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCWebPresentationLayer.Models.Query
+{
+    public class CarteiraQueryViewModel
+    {
+        public string NomeUsuario { get; set; }
+        public double Saldo { get; private set; }
+    }
+}
